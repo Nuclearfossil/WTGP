e2bfa4d626cdff8a57a99e49663129fa623b4ed2
Ash Matheson <Nuclearfossil@users.noreply.github.com>
2024-11-17 16:32:09 -0800

    Shader Clean-up

--- 08_SimpleLighting/shaders/SimpleLit.hlsl
+++ 08_SimpleLighting/shaders/SimpleLit.hlsl
@@ -36,15 +36,17 @@ VS_Output vs_main(VS_Input input)
     return output;
 }
 
+static const float4 minColor = (0.0, 0.0, 0.0, 0.0);
+static const float4 maxColor = (1.0, 1.0, 1.0, 1.0);
+
 // Simple Ambient + diffuse lighting model
 float4 ps_main(VS_Output input) : SV_TARGET
 {
-    float4 minColor = (0.0, 0.0, 0.0, 0.0);
-    float4 maxColor = (1.0, 1.0, 1.0, 1.0);
     float4 ambient = input.color * .1;
-    float3 lightPosition = position;
-    float3 l = normalize(lightPosition - input.worldpos);
-    float ndotl = max(dot(l, input.normal), 0.0f);
-    float4 lambertCont = input.color * ndotl;
+
+    float3 lightDir = normalize(position - input.worldpos);
+    float diffuse = saturate(dot(input.normal, lightDir)); // this is the 'intensity' of the light
+    float4 lambertCont = input.color * diffuse;
+
     return clamp(lambertCont + ambient, minColor, maxColor);
 }
