03d13c8c83a9858028d294d3ba3c1faa65cd05a9
Ash Matheson <Nuclearfossil@users.noreply.github.com>
2024-12-15 16:42:41 -0800

    fixing comments

--- 09_TextureMapping/graphics/Material.cpp
+++ 09_TextureMapping/graphics/Material.cpp
@@ -3,7 +3,7 @@
 #define STB_IMAGE_IMPLEMENTATION
 #include <stb_image.h>
 
-#include <filesystem> // Forces us to be C++ 17
+#include <filesystem> // for getting at current working directory and path operations. Forces us to C++17
 
 #include "utils.h"
 #include "framework.h"
